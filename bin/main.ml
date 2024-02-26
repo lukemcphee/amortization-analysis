698f977a00d2f5e20f747c96347ca1944e6da423
luke <luke@debian>
2024-02-26 20:16:37 +0000

    Interest rate plots working correctly

--- bin/main.ml
+++ bin/main.ml
@@ -2,13 +2,12 @@ module B = PrintBox
 open Mortgage__Types
 (* open Core *)
 
-let principal = 427000.0
+let principal = 1000000.0
 let term = 25
 let rows = Mortgage.calculate_rows
 (* use property_value if you want to override the vaule used to calculate loan-to-value rate if nothing is provided initial principal is used, if interest_rate is provided, this won't do anything. *)
 (* use interest_rate to use a static interest rate (this will invalidate any property_value input, as this is only used to calculate floating rates *)
-             (* ~property_value: None *)
-             ~property_value: (Some 600000.0)
+             (* ~property_value: (Some 1500000.0) *)
              (* ~interest_rate: (Some 0.05) *)
              principal
              term
--- lib/plot.ml
+++ lib/plot.ml
@@ -11,11 +11,8 @@ let graph rows =
            float_of_int @@ ((year * 12) + month))
          rows
   in
-  (* let interest_rate_by_month = Array.of_list @@ List.map (fun row -> row.interest_rate) rows in *)
-  (* let y_axis_interest_rate = *)
-    (* Mat.of_array interest_rate_by_month (Array.length interest_rate_by_month) 1 *)
-  (* in *)
   let x_axis = Mat.of_array x_axis_months (Array.length x_axis_months) 1 in
+
 
   let end_balance_by_month =
     Array.of_list @@ List.map (fun row -> row.end_period_balance) rows in
@@ -30,27 +27,34 @@ let graph rows =
   let y_axis_principle =
     Mat.of_array principle_by_month (Array.length principle_by_month) 1 in
   
-  let h = Plot.create ~m:3 ~n:2 "output.pdf" in
-  Plot.subplot h 0 0;
-  (* Plot.set_foreground_color h 0 50 255; *)
-  (* Plot.set_background_color h 0 0 0; *)
+  let h = Plot.create ~m:3 ~n:1 "output.pdf" in
   Plot.set_page_size h 1000 1000;
+
+  Plot.subplot h 0 0;
   Plot.(
     plot
       ~h
-      ~spec:[ RGB (255, 0, 0); LineStyle 1; Marker "#[0x2299]"; MarkerSize 8. ]
-      x_axis
-      y_axis_end_balance);
+      ~spec:[ RGB (255, 0, 0); LineStyle 1; Marker "#[0x2299]"; MarkerSize 8. ] x_axis y_axis_end_balance);
   Plot.(legend_on h ~position:NorthEast [| "remaining-balance" |]);
+
   Plot.subplot h 1 0;
   Plot.(scatter ~h ~spec:[ RGB (255, 0, 0); LineStyle 1 ] x_axis y_axis_interest);
   Plot.(scatter ~h ~spec:[ RGB (255, 0, 0); LineStyle 2 ] x_axis y_axis_principle);
   Plot.(legend_on h ~position:East [| "interest"; "principle_payment" |]);
 
-  (* Plot.subplot h 2 0; *)
-  (* let _ = print_endline @@ string_of_int (Array.length interest_by_month) in *)
-  (* Plot.(scatter ~h ~spec:[ RGB (255, 0, 0); LineStyle 1 ] x_axis y_axis_principle); *)
-  (* Plot.(legend_on h ~position:East [| "interest rate" |]); *)
+
+  (* only really worth commenting in the below if you're using sliding interest rate *)
+  (* if not, plplot throws an error as it doesn't know how to set y-scale. There's probably an easy solution      to this, I just haven't had time to figure it out                                               *)
+  let interest_rate_by_month = Array.of_list @@ List.map (fun row -> row.interest_rate *. 10000.) rows in
+  (* let interest_rate_by_month = [|4.;4.;4.|] in *)
+  let y_axis_interest_rate =
+    Mat.of_array interest_rate_by_month (Array.length interest_rate_by_month) 1
+  in
+  Plot.subplot h 2 0;
+  let _ = print_endline @@ string_of_int (Array.length interest_rate_by_month) in
+  let _ = Array.map (fun x -> print_endline @@ string_of_float x) interest_rate_by_month in 
+  Plot.(scatter ~h ~spec:[ RGB (255, 0, 0); LineStyle 1 ] x_axis y_axis_interest_rate);
+  Plot.(legend_on h ~position:East [| "interest rate by month" |]);
 
   Plot.output h
 ;;
